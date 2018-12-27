83c6c404250b4aeb802ddf5f56deab1fd14e8c93
chutrieukhang <jaychuu69@gmail.com>
2018-12-27 10:41:56 -0800

    Update bai 3 de 3.cpp

---
+++ bai 3 de 3.cpp
@@ -0,0 +1,28 @@
+/* ChonDe()
+chon de lam
+DocDe()
+doc dong 1
+Su dung for
+nhap cau tra loi ( A,B,C)
+int Kiemtra DapAn ( string a, string)
+return 2
+else return 0
+LuuDiem(int Diem)
+--------------------------------
+struct bode
+{
+	int SL
+	string cau hoi
+	string ct1,ct2,ct3,ct4
+struct ng{
+	string ten
+	int dem;
+}
+void docde(){
+	cout' cin..
+	in(de ==1)
+	{
+	ifstream ("de1.txt")
+	string s; bode de1;
+	getline(s,f); atoi(s);
+	*/
