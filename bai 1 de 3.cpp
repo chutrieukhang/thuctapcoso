f8ea802a682852c7959cba0fdde9228d7e065ceb
chutrieukhang <jaychuu69@gmail.com>
2018-12-27 10:41:38 -0800

    Update bai 1 de 3.cpp

--- bai 1 de 3.cpp
+++ bai 1 de 3.cpp
@@ -1,96 +1,158 @@
-#include <string.h>
-#include <fstream>
-#include <stdlib.h>
-#include <stack>
-#include <vector>
+#include<iostream >
+#include<string.h>
+#include<fstream>
+#include<vector>
+#include<stdlib.h>
 using namespace std;
-ifstream f("D:/Nhaptufile.txt");
-ofstream g("D:/Xuattufile.txt"); /*truy xuat du lieu*/
-class DB{
-	public:
-		string Ten;
-		string Gioitinh;
-		string SDT;
-		string Email;
-		string DiaChi;
-			Nhaptufile(){
-				string s;
-				getline(f,s); Ten = s;
-				getline(f,s); Gioitinh = s;
-				getline(f,s); SDT = s;
-				getline(f,s); Email = s;
-				getline(f,s); DiaChi = s;
-			}
-			NhapDB(){
-				cout << "Nhap ten: "; cin>>Ten;
-				cin.sync();
-				cout <<"Nhap gioi tinh"; cin>> Gioitinh;
-				cin.sync();
-				cout << " Nhap sdt "; cin>> SDT;
-				cin.sync();
-				cout << " Nhap email "; cin>> Email;
-				cin.sync();
-				cout <<" Nhap dia chi "; cin>> DiaChi;
-				cin.sync();
-			}
-			Xuattufile(){
-				g<< Ten << "\t" << GioiTinh <<"\t" <<SDT << "\t" <<Email << "\t";
-			}
-			Xuat()
-			{
-				cout << Ten << "\t" << GioiTinh <<"\t" <<SDT << "\t" <<Email << "\t";
-			}
-};
-class DBDT{
-	vectoc <DB>H;
-	vectpr <DB>K;
-	public:
-	void Nhap DB(){
-		int n;
-		string s;
-		getline(f,s); n=atoi(s.c_str());
-		H.resize (n);
-		for(int i=0 ; i<H.size();i++)
-			H[i].Nhaptufile();
-	}
-	void Them(){
-		DB d;
-		int n;
-		cin>> n;
-		while ( n== 0){
-			d.NhapDB();
-			d.Xuat();
-			H.push_back(d);
-			K.push_back(d);
-		}
-	}
-	void XuatThem(){
-		printf(K);
-	}
-	void Xoa(){
-		string s;
-		getline(cin,s);
-		for(int i=0; i < H.size(); i++)
-			if(k.compare(H[i].Ten) ==0 )
-				H[i].Xuat();
-	}
-	void Tim (){
-		string k;
-		getline(std::cin,k);
-		for ( int i = 0; i < H.size() ; i++)
-			if(k.compare (H[i].Ten)==0)
-				H[i].Xuat();
-	}
-	void chon(){
-		int n;
-		cin >> n;
-		cout <<" Nhap gia tri N =";
-	}
-};
-int main
+typedef struct 
 {
-	DBDT db;
-	db.nhapDB();
-	db.Them();
-	db.Tim();
+	char ten[50];
+	char gioitinh[5];
+	char sdt[11];
+	char email[30];
+	char diachi[50];
+}danhba;
+vector<danhba>db;
+void docfile()
+{
+	db.clear();
+	FILE *f;
+	f=fopen("d:\\danhba.dat","rb");
+	if(f!=NULL)
+	{
+		while(!feof(f))
+		{
+			danhba c;
+			fread(&c,sizeof(c),1,f);
+			db.push_back(c);
+		}	
+			fclose(f);
+	db.pop_back();
+	}
+	
+}
+void ghifile()
+{
+		danhba tam;
+		int size=db.size();
+		FILE *f;
+	f=fopen("d:\\danhba.dat","wb");
+	for(int i=0;i<size;i++)
+	{
+		tam=db[i];
+		fwrite(&tam,sizeof(tam),1,f);
+	}
+	fclose(f);
+}
+void themmoi(danhba c)
+{
+	db.push_back(c);
+		ghifile();	
+
+}
+void indanhba(danhba c)
+{
+	cout<<"so dien thoai "<<c.sdt<<endl;
+	cout<<"ten "<<c.ten<<endl;
+	cout<<"diachi "<<c.diachi<<endl;
+	cout<<"email "<<c.email<<endl;
+	cout<<"gioi tinh "<<c.gioitinh<<endl;
+	cout<<"............................."<<endl;
+}
+void indb()
+{
+	int size=db.size();
+	for(int i=0;i<size;i++)
+	{
+		indanhba(db[i]);
+	}
+}
+int tim(char sdt[11])
+{
+for(int i=0;i<db.size();i++)
+{
+	if(strcmp(db[i].sdt,sdt)==0)
+	return i;
+	 return -1;	
+	 	
+}
+}
+void sua (char sdt[11])
+{
+	int vt=tim(sdt);
+	
+	db.erase(db.begin()+vt);
+	danhba c;
+if(vt>=0){
+
+		cout<<"nhap lai danh ba"<<endl<<"ten nguoi dung";
+		cin.sync();
+		cin>>c.ten;
+		cout<<"nhap sdt";
+		cin.sync();
+		cin>>c.sdt;
+		cout<<"nhap email";
+		cin.sync();
+		cin>>c.email;
+		cout<<"nhap gioi tinh";
+		cin.sync();
+		cin>>c.gioitinh;
+		cout<<"nhap dia chi";cin.sync();
+		cin>>c.diachi;
+		db.insert(db.begin()+vt ,c);
+}
+else
+cout<<"khong thay sdt can tim";
+			
+}
+void xoa(char sdt[11])
+{
+//	int vt=tim(sdt);
+//	if(vt>=0)
+//	{
+//	db.erase(db.begin()+vt);
+//	}
+//	else
+//	cout<<"khong thay sdt can tim";
+for(int i=0;i<db.size();i++)
+{
+	if(strcmp(db[i].sdt,sdt)==0)
+		{
+		db.erase(db.begin()+i);	
+			
+		}
+}
+}
+int timtheoten(char ten[50])
+{
+for(int i=0;i<db.size();i++)
+{
+	if(strcmp(db[i].ten,ten)==0)
+	return i;
+	else return -1;	
+	
+}
+}
+int main()
+{
+	fflush(stdin);
+	danhba c;
+	strcpy(c.ten,"chu trieu khang");
+	strcpy(c.email,"dbbb");
+	strcpy(c.diachi,"nha trang");
+	strcpy(c.gioitinh,"nam");
+	strcpy(c.sdt,"0927171924");
+	themmoi(c);	
+	danhba c1;
+	strcpy(c1.ten, "chu trieu khang1	");
+	strcpy(	c1.diachi,"nha trang");
+	strcpy(	c1.email,"dbbb");
+	strcpy(c1.gioitinh,"nam");
+	strcpy(c1.sdt,"1");
+	themmoi(c1);	
+	ghifile();
+	sua("0927171924");
+	xoa("1");
+	indb();
+	cout<<timtheoten("khong");
 }
